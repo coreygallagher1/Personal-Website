537d0629db3a99a254a985e4b1565bf73a95319b
Corey Gallagher <coreygallagher1194@gmail.com>
2022-05-03 19:38:49 -0500

    added templates for pages

--- home/views.py
+++ home/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is my homepage (/)")
-
+    #return HttpResponse("This is my homepage (/)")
+    return render(request, 'home.html')
 def about(request):
-    return HttpResponse("This is my about page (/about)")
-
+    #return HttpResponse("This is my about page (/about)")
+    return render(request, 'about.html')
 def projects(request):
-    return HttpResponse("This is my projects page (/projects)")
-
+    #return HttpResponse("This is my projects page (/projects)")
+    return render(request, 'projects.html')
 def contact(request):
-    return HttpResponse("This is my contacts page (/contact)")
+    #return HttpResponse("This is my contacts page (/contact)")
+    return render(request, 'contact.html')
